f2674628ab3c8957cc7f3f964e8ece7702000c9a
darexxel <darexxel@gmail.com>
2025-02-11 23:20:14 +0100

    chore: add test comment in books.py to trigger CI pipeline

--- api/routes/books.py
+++ api/routes/books.py
@@ -36,3 +36,5 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+# This is a test comment to trigger the CI pipeline.
